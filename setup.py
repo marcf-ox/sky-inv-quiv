ec38ddf06618a884a80adac7c70926233cfde5dd
marcf <46935253+marcf-ox@users.noreply.github.com>
2025-10-21 15:30:54 +0100

    Update numpy version requirement in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     author='Marcf',
     license='MIT',
-    install_requires=['numpy>=2.4.1','scipy>=1.10.0','cfractions>=2.4.1','networkx>=2.6.2','matplotlib>=3.10.7'],
+    install_requires=['numpy>=2.3.1','scipy>=1.10.0','cfractions>=2.4.1','networkx>=2.6.2','matplotlib>=3.10.7'],
     python_requires=">=3.8.0",
     packages=find_packages(),
 )
